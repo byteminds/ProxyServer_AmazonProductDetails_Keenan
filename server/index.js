45d4ce17b72f7963220023d10817392c4ca0f158
Keenan Thomson <keenanthomson@gmail.com>
2019-07-13 15:58:29 -0400

    Work on refactor to serve by id; CORB issue w client

--- server/index.js
+++ server/index.js
@@ -2,11 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const port = 3000;
+const cors = require('cors')
 
 let app = express();
 
+app.use(cors());
 app.use(express.static(path.join(__dirname, '../public')));
+app.use('/:id', express.static(path.join(__dirname, '../public'))) // figure out why id route not working
+// also debug CORB issue client side
+
+
+
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.get('http://localhost:3004/:id', (req, res) => {
+  // console.log(req.url);
+//   res.send(data);
+// })
 
 const server = app.listen(port, () => console.log(`Proxy server listening on port ${port}`));
 

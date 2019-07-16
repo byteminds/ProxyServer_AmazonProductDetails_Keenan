234611127551a75e6a50e0b56d48ae82963664ad
Keenan Thomson <keenanthomson@gmail.com>
2019-07-16 00:33:05 -0400

    Get 3 services to render with API collision issue to fix

--- server/index.js
+++ server/index.js
@@ -2,23 +2,45 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const port = 3000;
-const cors = require('cors')
+const axios = require('axios');
+// const cors = require('cors');
 
 let app = express();
 
-app.use(cors());
-app.use(express.static(path.join(__dirname, '../public')));
-app.use('/:id', express.static(path.join(__dirname, '../public'))) // figure out why id route not working
-// also debug CORB issue client side
-
-
-
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/:id', express.static(path.join(__dirname, '../public')));
 
-// app.get('http://localhost:3004/:id', (req, res) => {
-  // console.log(req.url);
-//   res.send(data);
-// })
+//get product reviews component ****DEDUPE /api/id route causing collision(?)******
+app.use('/api/:productId', (req, res) => {
+  const url = `http://localhost:3001/api/${req.params.productId}`;
+  axios.get(url).then((response) => {res.send(response.data)});
+});
+
+app.use('/api/:productId/summary', (req, res) => {
+  const url = `http://localhost:3001/api/${req.params.productId}/summary`;
+  axios.get(url).then((response) => {res.send(response.data)});
+});
+
+
+
+// get product description component and fonts
+app.use('/api/:id', (req, res) => {
+  const url = `http://localhost:3003/api/${req.params.id}`;
+  axios.get(url).then((response) => {res.send(response.data)});
+});
+
+app.use('/:id/font/:url', (req, res) => {
+  const url = `http://localhost:3003/${req.params.id}/font/${req.params.url}`;
+  axios.get(url).then((response) => {res.send(response.data)});
+  console.log(`fonts sent!`)
+});
+
+
+// get pricing component
+app.get('/api/:id', (req, res) => {
+  const url = `http://localhost:3004/pricingAPI/${req.params.id}`;
+  axios.get(url).then((response) => {res.send(response.data)});
+});
 
 const server = app.listen(port, () => console.log(`Proxy server listening on port ${port}`));
 
